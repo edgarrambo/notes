1ea9be2dee94c6e4cf2e8fcf5dd62ad2dd11aac0
edgarrambo <ragde956@gmail.com>
2020-03-07 00:46:09 -0600

    apis linked

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note';
+import axios from 'axios';
 
 
-function App() {
-  return (
-    <div className="App">
-          <Nav />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
 
-    </div>
-  );
+  toggleNote = () => {
+    this.setState({
+      showNote: ! this.state.showNote
+    });
+  }
+
+  getNotes = () => {
+    axios.get('https://firehose-note-api.herokuapp.com/notes')
+    .then((res) => console.log(res.data) )
+    .catch((err) => console.log(err.response.data) );
+
+
+
+  }
+
+  render() {
+    const { showNote } = this.state;
+
+    return (
+      <div className="App">
+      <Nav toggleNote={this.toggleNote} showNote={showNote} />
+
+        { showNote ? <Note /> : <List getNotes={this.getNotes}/> }
+
+      </div>
+    );
+  }
 }
 
 export default App;
