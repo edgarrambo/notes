b85ac6a6f6dc02d3d2f8a2f6f4bfedf85cfb6ce6
edgarrambo <ragde956@gmail.com>
2020-03-07 22:52:29 -0600

    notes can edit, submit, and cancel

--- src/App.js
+++ src/App.js
@@ -13,38 +13,63 @@ class App extends Component {
     super();
     this.state = {
       showNote: false,
-      notes: []
+      notes: [],
+      note: {}
     };
   }
 
   toggleNote = () => {
     this.setState({
-      showNote: ! this.state.showNote
-    });
+      showNote: ! this.state.showNote,
+      note: {}
+        });
   }
 
   getNotes = () => {
     axios.get(urlFor('notes'))
     .then((res) => this.setState({ notes: res.data }) )
     .catch((err) => console.log(err.response.data) );
+  }
 
+  getNote = (id) => {
+    axios.get(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ note: res.data, showNote: true }) )
+    .catch((err) => console.log(err.response.data) );
+  }
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data);
+    } else {
+      return axios.post(urlFor('notes'), data);
+    }
+    }
 
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)
+    .then((res) => this.setState({ showNote: false }) )
+    .catch((err) => console.log(err.response.data) );
 
   }
 
   render() {
-    const { showNote, notes } = this.state;
+    const { showNote, notes, note } = this.state;
 
 
     return (
       <div className="App">
       <Nav toggleNote={this.toggleNote} showNote={showNote} />
       {showNote ?
-        <Note />
+        <Note
+        note={note}
+        submitNote={this.submitNote}
+
+        />
         :
         <List
           getNotes={this.getNotes}
           notes={notes}
+          getNote={this.getNote}
+
         />
       }
       </div>
