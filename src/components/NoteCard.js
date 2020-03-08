ef236ca56d08c70027e1f2953f29ad264a07b011
edgarrambo <ragde956@gmail.com>
2020-03-08 16:17:04 -0500

    app finished, tested for bugs

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
 class NoteCard extends React.Component {
+  renderTags(note) {
+    return note.tags.map((tag, index) =>
+    <span className="note-card-tag" key={index}>
+      {tag.name}
+    </span>
+  );
+  }
     render() {
         const { note, getNote, deleteNote} = this.props;
 
         return (
-            <div className="note-card-container">
-                <div className="note-card-title">
+        <div className="note-card-container">
+        <div className="note-card-title">
                     {note.title}
         </div>
         <div className="note-card-content">
           {note.content}
         </div>
+        <div className="note-card-tags">
+        {this.renderTags(note)}
+      </div>
         <span className="note-card-delete" onClick={() => deleteNote(note.id)}>
           <i className="material-icons">close</i>
         </span>
